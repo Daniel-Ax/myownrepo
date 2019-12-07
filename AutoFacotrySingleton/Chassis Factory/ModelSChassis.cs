e81cfa7aa6f58ef874371a0c350c5af16ac52411
finyw <finyw@DESKTOP-NOMHP5D>
2019-12-07 13:59:15 +0100

    SZt

---
+++ AutoFacotrySingleton/Chassis Factory/ModelSChassis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFacotrySingleton.Chassis_Factory
+{
+    public class ModelSChassis:Chassis
+    {
+        public ModelSChassis()
+        { 
+            
+        }
+    }
+}
---
+++ AutoFacotrySingleton/Chassis Factory/ModelYChassis.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFacotrySingleton.Chassis_Factory
+{
+    public class ModelYChassis:Chassis
+    {
+        public ModelYChassis()
+        { 
+        
+        }
+            
+    }
+}
