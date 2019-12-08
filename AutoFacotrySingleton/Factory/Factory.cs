773d3cf81ae8147ab05b53dc774a4e10fac629ca
Ács Dániel <finywow@gmail.com>
2019-12-08 12:06:00 +0100

    Factory

--- AutoFacotrySingleton/Factory/Factory.cs
+++ AutoFacotrySingleton/Factory/Factory.cs
@@ -8,6 +8,9 @@ namespace AutoFacotrySingleton.Factory
 {
     public abstract class Factory
     {
-       
+        public abstract void Run(int amount);
+        public abstract void CreateCarBody();
+        public abstract void CreateChassis();
+        public abstract void CreateEngine();
     }
 }
