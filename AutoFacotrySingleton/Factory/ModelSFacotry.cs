9abc3bf4de82b68c09669499703a2ce36e0372b0
Ács Dániel <finywow@gmail.com>
2019-12-09 15:08:56 +0100

    AbstractFactory

--- AutoFacotrySingleton/Factory/ModelSFacotry.cs
+++ AutoFacotrySingleton/Factory/ModelSFacotry.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoFacotrySingleton.Car_Body_Facotry;
+using AutoFacotrySingleton.Chassis_Factory;
+using AutoFacotrySingleton.Engine_Factory;
 
 namespace AutoFacotrySingleton.Factory
 {
-    class ModelSFacotry
+    class ModelSFacotry : IFacotry
     {
+        public CarBody CreateCarBody()
+        {
+            return new ModellSCarBody();
+        }
+
+        public Chassis CreateChassis()
+        {
+            return new ModelSChassis();
+        }
+
+        public Engine CreateEngine()
+        {
+            return new ModelSEngine();
+        }
     }
+
 }
--- AutoFacotrySingleton/Factory/ModelYFactory.cs
+++ AutoFacotrySingleton/Factory/ModelYFactory.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoFacotrySingleton.Car_Body_Facotry;
+using AutoFacotrySingleton.Chassis_Factory;
+using AutoFacotrySingleton.Engine_Factory;
 
 namespace AutoFacotrySingleton.Factory
 {
-    class ModelYFactory
+    class ModelYFactory : IFacotry
     {
+        public CarBody CreateCarBody()
+        {
+            return new ModelYCarBody();
+        }
+
+        public Chassis CreateChassis()
+        {
+            return new ModelYChassis();
+        }
+
+        public Engine CreateEngine()
+        {
+            return new ModelYEngine();
+        }
     }
 }
