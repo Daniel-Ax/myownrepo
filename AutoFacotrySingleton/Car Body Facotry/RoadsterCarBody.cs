652fb8edb0ef7a4c9776ea596908b6a21cb611b6
Ács Dániel <finywow@gmail.com>
2019-12-11 11:16:43 +0100

    Roadster CarBody

---
+++ AutoFacotrySingleton/Car Body Facotry/RoadsterCarBody.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AutoFacotrySingleton.Car_Body_Facotry;
+using AutoFacotrySingleton.Chassis_Factory;
+using AutoFacotrySingleton.Engine_Factory;
+
+namespace AutoFacotrySingleton.CarBodyFacotry
+{
+    public class RoadsterCarBody:CarBody
+    {
+        public override void CreateFenders()
+        {
+            Console.WriteLine("Created Roadster Fenders");
+        }
+
+        public override void CreateHood()
+        {
+            Console.WriteLine("Crated Roadster Hood");
+        }
+
+        public override void CreateSunRoof()
+        {
+            Console.WriteLine("Created Roadster Sunroof");
+        }
+    }
+}
